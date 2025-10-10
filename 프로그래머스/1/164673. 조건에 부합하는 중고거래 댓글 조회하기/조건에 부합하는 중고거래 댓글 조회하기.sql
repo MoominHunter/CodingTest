c448b2e5183a286b96e5d274ff6fb6776387e16d
Sehyun Oh <141246408+MoominHunter@users.noreply.github.com>
2025-10-11 00:05:40 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,22 @@
+-- USED_GOODS_BOARD: BOARD_ID, WRITER_ID, TITLE, CONTENTS, PRICE, CREATED_DATE, STATUS, VIEWS
+-- USED_GOODS_REPLY: REPLY_ID, BOARD_ID, WRITER_ID, CONTENTS, CREATED_DATE
+
+-- 2022년 10월에 작성된 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일
+-- 댓글 작성일 오름차순, 게시글 제목 오름차순
+
+SELECT b.TITLE
+        , b.BOARD_ID
+        , r.REPLY_ID
+        , r.WRITER_ID
+        , r.CONTENTS
+        , DATE_FORMAT(r.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE
+        
+FROM USED_GOODS_BOARD AS b
+    JOIN USED_GOODS_REPLY AS r
+    ON b.BOARD_ID = r.BOARD_ID
+    
+WHERE (year(b.CREATED_DATE) LIKE 2022)
+    AND (month(b.CREATED_DATE) LIKE 10)
+    
+ORDER BY r.CREATED_DATE, b.TITLE
+;
