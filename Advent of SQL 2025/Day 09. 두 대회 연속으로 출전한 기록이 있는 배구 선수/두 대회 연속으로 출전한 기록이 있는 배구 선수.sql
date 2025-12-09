d0d63aad915f6ec3badf1ccd4bbc7d52e4db927b
Sehyun Oh <141246408+MoominHunter@users.noreply.github.com>
2025-12-09 23:59:47 +0900

    Add SQL query for volleyball player records

---
+++ Advent of SQL 2025/Day 09. 두 대회 연속으로 출전한 기록이 있는 배구 선수/두 대회 연속으로 출전한 기록이 있는 배구 선수.sql
@@ -0,0 +1,24 @@
+WITH base AS (
+  SELECT
+    r.athlete_id AS id,
+    a.name,
+    g.year,
+    LAG(g.year) OVER (PARTITION BY r.athlete_id ORDER BY g.year) AS prev_year
+  FROM records r
+  JOIN athletes a ON r.athlete_id = a.id
+  JOIN games g ON g.id = r.game_id
+  JOIN teams t ON t.id = r.team_id
+  JOIN events e ON e.id = r.event_id
+  WHERE
+    t.team = 'KOR'
+    AND e.event = 'Volleyball Women''s Volleyball'
+    AND g.year <= 2016
+)
+SELECT DISTINCT
+  id,
+  name
+FROM base
+WHERE
+  prev_year IS NOT NULL
+  AND year - prev_year = 4
+ORDER BY id;
